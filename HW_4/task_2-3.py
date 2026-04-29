cb53337d89e6dd100f2e9864c920df2ac18bae97
IrynDenysova <iriska.ermak@gmail.com>
2026-04-29 15:07:08 +0200

    fix 2-3 task

--- HW_4/task_2-3.py
+++ HW_4/task_2-3.py
@@ -2,14 +2,17 @@
 # Извлеките все записи из таблицы categories.
 # Для каждой категории извлеките и выведите все связанные с ней продукты,
 # включая их названия и цены.
+
 from task_1 import *
 
-categories = session.query(Category).all()
-for category in categories:
-    print(category.name)
-    for product in category.products:
-        print(f"- {product.name}: ${product.price:.2f}")
+data = session.query(Category, Product).join(Product).all()
+for category, product in data:
+    print(f"{category.name}-{product.name}: ${product.price}")
 
+#     print(category.name)
+#     for product in category.products:
+#         print(f"- {product.name}: ${product.price:.2f}")
+#
 # Задача 3: Обновление данных.
 # Найдите в таблице products первый продукт с названием "Смартфон". Замените цену этого продукта на 349.99.
 
